c63e0aacc27bb39b5b05ad0842cb53f06060ee71
Dongbin Na / 나동빈 <ndb796@naver.com>
2022-07-03 15:58:26 +0900

    Update 4344.java

--- 4344.java
+++ 4344.java
@@ -25,7 +25,7 @@ public class Main {
             // 평균을 넘는 학생의 비율 계산
             double ratio = (double) count * 100 / n;
             // 소수점 아래 셋째자리까지 출력
-            String answer = String.format("%.5g", ratio);
+            String answer = String.format("%.3f", ratio);
             System.out.println(answer + "%");
         }
     }
