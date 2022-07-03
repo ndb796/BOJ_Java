6f8434499267654216f8d1fbd711e98d8e55e5e0
Dongbin Na / 나동빈 <ndb796@naver.com>
2022-07-03 14:09:43 +0900

    Create 18108.java

---
+++ 18108.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt(); // 불기 연도가 입력되었을 때
+        System.out.println(year - 543); // 서기 연도를 출력
+    }
+}
