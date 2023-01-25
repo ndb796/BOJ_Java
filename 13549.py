918cfc9ec6ea59e4229681d5168e7e8d652d5336
Dongbin Na / 나동빈 <ndb796@naver.com>
2023-01-25 21:56:39 +0900

    Update 13549.py

--- 13549.py
+++ 13549.py
@@ -28,3 +28,31 @@ while queue:
 
 
 print(visited[k] - 1)
+
+"""
+import heapq
+
+n, k = map(int, input().split())
+distance = [int(1e9)] * 100001
+
+q = []
+heapq.heappush(q, (0, n))
+distance[n] = 0
+
+while q:
+    dist, current = heapq.heappop(q)
+    if current - 1 >= 0:
+        if distance[current - 1] > dist + 1:
+            distance[current - 1] = dist + 1
+            heapq.heappush(q, (dist + 1, current - 1))
+    if current + 1 <= 100000:
+        if distance[current + 1] > dist + 1:
+            distance[current + 1] = dist + 1
+            heapq.heappush(q, (dist + 1, current + 1))
+    if current * 2 <= 100000:
+        if distance[current * 2] > dist:
+            distance[current * 2] = dist
+            heapq.heappush(q, (dist, current * 2))
+
+print(distance[k])
+"""
