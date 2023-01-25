2b458cf28783ce0bba5bc33d61f664c72f5a367a
Dongbin Na / 나동빈 <ndb796@naver.com>
2023-01-25 21:18:28 +0900

    Update 1697.java

--- 1697.java
+++ 1697.java
@@ -67,3 +67,48 @@ public class Main {
         }
     }
 }
+
+/*
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static int[] visited = new int[100001];
+  public static int n, k;
+  
+  public static void main(String[] args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+
+    n = scanner.nextInt();
+    k = scanner.nextInt();
+
+    Queue<Integer> q = new LinkedList<>();
+    visited[n] = 1;
+    q.offer(n);
+    while (!q.isEmpty()) {
+      int current = q.poll();
+      if (current - 1 >= 0) {
+        if (visited[current - 1] == 0) {
+          q.offer(current - 1);
+          visited[current - 1] = visited[current] + 1;
+        }
+      }
+      if (current + 1 <= 100000) {
+        if (visited[current + 1] == 0) {
+          q.offer(current + 1);
+          visited[current + 1] = visited[current] + 1;
+        }
+      }
+      if (current * 2 <= 100000) {
+        if (visited[current * 2] == 0) {
+          q.offer(current * 2);
+          visited[current * 2] = visited[current] + 1;
+        }
+      }
+    }
+
+    System.out.println(visited[k] - 1);
+  }
+}
+*/
